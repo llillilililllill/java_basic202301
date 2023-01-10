761c2048de49d0741672f9909dc299da8ae3ae02
llillilililllill <skenals12@gmail.com>
2023-01-10 16:06:41 +0900

    # 다형성 예제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.basic.Dancer;
 import org.example.inherit.StreetDancer;
 import org.example.inherit.idolDancer;
+import org.example.poly.PerformanceTeam;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,5 +20,18 @@ public class Main {
         System.out.println();
 
         streetDancer.dance();
+
+        Dancer idoldancer1 = new idolDancer("아이돌팀1", "아이돌1");
+        Object idoldancer2 = new idolDancer("아이돌팀2", "아이돌2");
+        idolDancer idoldancer3 = new idolDancer("아이돌팀3", "아이돌3");
+
+        Dancer Streetdancer1 = new StreetDancer("스트릿댄서1", "스트릿1");
+        Object Streetdancer2 = new StreetDancer("스트릿댄서2", "스트릿2");
+        StreetDancer Streetdancer3 = new StreetDancer("스트릿댄서3", "스트릿3");
+
+        PerformanceTeam Team = new PerformanceTeam();
+
+        Team.joinDancer(idoldancer1);
+        Team.joinDancer(Streetdancer1);
     }
 }
---
+++ src/main/java/org/example/poly/PerformanceTeam.java
@@ -0,0 +1,18 @@
+package org.example.poly;
+
+import org.example.basic.Dancer;
+import org.example.inherit.idolDancer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PerformanceTeam {
+
+    private List<Dancer> dancers = new ArrayList<>();
+
+    public void joinDancer(Dancer dancer) {
+        dancers.add(dancer);
+        System.out.println(dancer.getMyName() + "님이 팀에 가입했습니다.");
+        System.out.println("현재 팀원수: " + dancers.size() + "명");
+    }
+}
